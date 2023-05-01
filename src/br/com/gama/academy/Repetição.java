5d8cc8a319b057b4dc3ed98fecef70603c2e9c04
Carlos Aruh <112590838+CarlosAruh@users.noreply.github.com>
2023-05-01 17:22:08 -0300

    Adicionando Exercico 1

---
+++ src/br/com/gama/academy/Repetição.java
@@ -0,0 +1,13 @@
+package br.com.gama.academy;
+
+public class Repetição {
+	public static void main(String[] args) {
+		for (int l = 0; l < 5; l++) { 
+		    for (int c = 0; c < 5; c++) { 
+		        System.out.print("* "); 
+		    }
+		    System.out.println(); 
+		}
+	}
+
+}
