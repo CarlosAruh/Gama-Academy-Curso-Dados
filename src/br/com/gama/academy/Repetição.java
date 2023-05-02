35351005b7fe16fd4647b79e833f9a71eaa490a1
CarlosAruh <carlos.aruh@gmail.com>
2023-05-01 21:04:54 -0300

    Coloquei 6 linhas e 6 colunas

--- src/br/com/gama/academy/Repetição.java
+++ src/br/com/gama/academy/Repetição.java
@@ -2,8 +2,8 @@ package br.com.gama.academy;
 
 public class Repetição {
 	public static void main(String[] args) {
-		for (int l = 0; l < 5; l++) { 
-		    for (int c = 0; c < 5; c++) { 
+		for (int l = 0; l <= 5; l++) { 
+		    for (int c = 0; c <= 5; c++) { 
 		        System.out.print("* "); 
 		    }
 		    System.out.println(); 
